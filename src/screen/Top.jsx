9549603d3ceae8cb7dcca855da6b72237734e444
Yash <yash.sharma0002@gmail.com>
2018-11-24 19:20:56 +0530

    Top Bar Component start

---
+++ src/screen/Top.jsx
@@ -0,0 +1,12 @@
+import TopBar from '../component/TopBar'
+import React from 'react';
+
+class Top extends React.Component {
+    render() {
+        return (
+            <TopBar />
+        )
+    }
+}
+
+exports default Top;
