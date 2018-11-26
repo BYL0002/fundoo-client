1b9cdc93ee8e498d390f3dda1bb1fcc944d404da
Yash <yash.sharma0002@gmail.com>
2018-11-26 10:55:38 +0530

    Registration start

---
+++ src/component/Login.jsx
@@ -0,0 +1,29 @@
+/**
+ * @description Login Component faciitates login
+ * @author Yash
+ * @since 26/11/2018
+ * @version 1.0
+ * @module component on client side
+ */
+import React from 'React';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.satte = {
+            email : ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+/**
+ * @exports Login component so as to get login by user
+ */
+export default Login;
