8eaa4a869d1583986f4bf6b347e2d22ae84ee7cc
Yash <yash.sharma0002@gmail.com>
2018-11-30 10:50:16 +0530

    token based dashboard opening

--- src/screen/Dashboard.jsx
+++ src/screen/Dashboard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import Topbar from '../component/Topbarc';
 
 class Dashboard extends React.Component {
     render() {
+        if( localStorage.getItem('userLogToken') === null) 
+        {
+            return <Redirect to = "/" />
+        }
         return (
             <Topbar />
         )
